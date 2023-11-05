132e2d6efdf5280039ef2d8f5adc874d20e3dded
Somin Park <ps4708@naver.com>
2023-11-05 22:58:13 +0900

    feat: 로또 구입 금액을 입력받는 기능 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String inputPurchaseAmount(){
+        String purchaseAmount =  Console.readLine();
+        return purchaseAmount;
+    }
+
+}
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,6 @@
+package lotto.view;
+
+public class OutputView {
+    private final static String PROMPT_ENTER_PURCHASE_AMOUNT="구입금액을 입력해 주세요.";
+
+}
