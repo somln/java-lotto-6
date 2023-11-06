a6149a55b0d291c20079d8e2c6468f55be282107
Somin Park <ps4708@naver.com>
2023-11-06 21:32:05 +0900

    feat: 구입 금액을 입력받는 메세지 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static String inputPurchaseAmount(){
+        OutputView.printPurchaseAmountInputPrompt();
         String purchaseAmount =  Console.readLine();
         return purchaseAmount;
     }
