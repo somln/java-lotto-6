ef1c2215b4ca5fcdaa7657269756eef973900d21
Somin Park <ps4708@naver.com>
2023-11-07 00:13:20 +0900

    feat: 보너스 번호를 입력받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,6 +2,8 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import javax.swing.*;
+
 public class InputView {
 
     public static String inputPurchaseAmount(){
@@ -11,9 +13,15 @@ public class InputView {
     }
 
     public static String inputWinnerNumbers(){
-        OutputView.printWinnerNumberInputPrompt();
+        OutputView.printWinningNumberInputPrompt();
         String winnerNumbers = Console.readLine();
         return winnerNumbers;
+    }
+
+    public static String inputBonusNumber(){
+        OutputView.printBonusNumberInputPrompt();;
+        String bonusNumber = Console.readLine();
+        return bonusNumber;
     }
 
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -7,7 +7,8 @@ import lotto.domain.Purchase;
 public class OutputView {
     private static final String PROMPT_ENTER_PURCHASE_AMOUNT="구입금액을 입력해 주세요.";
     private static final String PROMPT_LOTTO_TICKET_COUNT= "%d개를 구매했습니다.";
-    private static final String PROMPT_ENTER_WINNER_NUMBER= "당첨 번호를 입력해 주세요.";
+    private static final String PROMPT_ENTER_WINNING_NUMBER= "당첨 번호를 입력해 주세요.";
+    private static final String PROMPT_ENTER_BONUS_NUMBER= "보너스 번호를 입력해 주세요";
 
 
     public static void printPurchaseAmountInputPrompt(){
@@ -18,8 +19,12 @@ public class OutputView {
         System.out.printf(PROMPT_LOTTO_TICKET_COUNT+ "%n", purchase.getTicketCount());
     }
 
-    public static void printWinnerNumberInputPrompt(){
-        System.out.println(PROMPT_ENTER_WINNER_NUMBER);
+    public static void printWinningNumberInputPrompt(){
+        System.out.println(PROMPT_ENTER_WINNING_NUMBER);
+    }
+
+    public static void printBonusNumberInputPrompt(){
+        System.out.println(PROMPT_ENTER_BONUS_NUMBER);
     }
 
     public static void printLottos(Lottos lottos) {
