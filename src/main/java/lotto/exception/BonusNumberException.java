9fd7267a6d31fadd91683289255a4e7a7af5d882
Somin Park <ps4708@naver.com>
2023-11-07 00:14:12 +0900

    feat: 보너스 번호를 검증하는 기능 추가

---
+++ src/main/java/lotto/exception/BonusNumberException.java
@@ -0,0 +1,8 @@
+package lotto.exception;
+
+public class BonusNumberException extends IllegalArgumentException{
+
+    public BonusNumberException(String message){
+        super(message);
+    }
+}
