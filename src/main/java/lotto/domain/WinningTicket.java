882a7f22a663f6e6b5d1efc22fb8d08e66bf22e7
Somin Park <ps4708@naver.com>
2023-11-07 15:22:40 +0900

    feat: 당첨 번호와 보너스 번호를 저장하는 기능 추가

---
+++ src/main/java/lotto/domain/WinningTicket.java
@@ -0,0 +1,47 @@
+package lotto.domain;
+
+import lotto.exception.BonusNumberException;
+
+import java.util.List;
+
+import static lotto.domain.LottoConst.MAX_NUMBER;
+import static lotto.domain.LottoConst.MIN_NUMBER;
+import static lotto.utill.ErrorMessage.BONUS_NUMBER_OVERLAP;
+import static lotto.utill.ErrorMessage.NUMBER_RANGE;
+
+public class WinningTicket {
+
+    private final Lotto winningLotto;
+    private final int bonusNumber;
+
+    public WinningTicket(lotto.domain.Lotto winningLotto, int bonusNumber) {
+        validateBonusNumber(winningLotto, bonusNumber);
+        this.winningLotto = winningLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    public void validateBonusNumber(Lotto lotto, int bonusNumber){
+        validateNumberRange(bonusNumber);
+        validateBonusOverlap(lotto, bonusNumber);
+    }
+    private static void validateNumberRange(int bonusNumber) {
+        if(!isNumberInRange(bonusNumber)) {
+            throw new BonusNumberException(String.format(NUMBER_RANGE.getMessage(), MIN_NUMBER, MAX_NUMBER));
+        }
+    }
+
+    private static boolean isNumberInRange(int bonusNumber) {
+        return (bonusNumber >= MIN_NUMBER && bonusNumber <= MAX_NUMBER);
+    }
+
+    private static void validateBonusOverlap(Lotto lotto, int bonusNumber) {
+        if(isAlreadyInWinningNumbers(lotto, bonusNumber)){
+            throw new BonusNumberException(BONUS_NUMBER_OVERLAP.getMessage());
+        }
+    }
+
+    private static boolean isAlreadyInWinningNumbers(Lotto lotto, int bonusNumber){
+        List<Integer> winningNumbers = lotto.getNumbers();
+        return winningNumbers.contains(bonusNumber);
+    }
+}
